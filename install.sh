6166b45b5e1fc978dfdad5390afb604c3991fbe3
Javier Montejo <jmontejo@cern.ch>
2021-10-11 17:28:42 +0200

    inst

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@ git submodule update
 #tar -xzf SPheno-4.0.5.tar.gz
 
 #Get some new python3
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-setupATLAS
+which lsetup &> /dev/null || setupATLAS
 lsetup "views LCG_100 x86_64-centos7-gcc8-opt"
 
 ### Link RPV models (more are available but not linked)
