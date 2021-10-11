92b232ec82343cc9c93216208bbfebac6230e586
Javier Montejo <jmontejo@cern.ch>
2021-10-11 17:02:36 +0200

    Up installwq

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
+git submodule init
+git submodule update
 #wget https://spheno.hepforge.org/downloads/?f=SPheno-4.0.5.tar.gz -O SPheno-4.0.5.tar.gz
 #tar -xzf SPheno-4.0.5.tar.gz
 
 #Get some new python3
 setupATLAS
 lsetup "views LCG_100 x86_64-centos7-gcc8-opt"
+
+### Link RPV models (more are available but not linked)
+cd SPheno/models
+ln -s ../../SARAH_models/Models/* .
 
 ### Install SPheno
 cd SPheno
@@ -13,9 +19,3 @@ cmake -DMODELS=MSSMTriRpV ..
 make -j8
 make install
 
-### Link RPV models (more are available but not linked)
-cd SPheno/models
-sarahmodelfolder="../../SARAH_models/Models/MSSM-RpV/"
-for model in `ls $sarahmodelfolder`; do 
-    ln -s $sarahmodelfolder/$model MSSM-RpV-$model
-done
