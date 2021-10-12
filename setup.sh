c503f96ae0b177bde3fc0bab7006f41a02b2c57d
Javier Montejo Berlingen <jmontejo@cern.ch>
2021-10-12 15:08:04 +0200

    Up

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export SPHENO_ROOT=`pwd`
 export PYTHONPATH="$PYTHONPATH:$SPHENO_ROOT/python"
-export PATH="$PATH:$SPHENO_ROOT/SPheno/bin"
+export SPHENO_PATH="/Users/JMontejo/Documents/Physics/LeptonMultijet/SPheno4"
+export PATH="$PATH:$SPHENO_PATH/bin"
